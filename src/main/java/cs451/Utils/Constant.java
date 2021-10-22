f4cbafde03e0f1038b5496cf9303231f4e5cc4d5
Zhiye Wang <493878030@qq.com>
2021-10-22 20:42:27 +0200

    1

--- src/main/java/cs451/Utils/Constant.java
+++ src/main/java/cs451/Utils/Constant.java
@@ -8,12 +8,12 @@ public class Constant {
     public static final String BROADCAST = "b";
     public static final String DELIVER = "d";
     public static final int RECEIVEINTERVAL = 5; //Interval between sending same message
-    public static final int SENDINTERVAL = 100;
+    public static final int SENDINTERVAL = 500;
     public static final int NOTFOUND = -1;
     public static final int BUFFER_SIZE = 1000000;
     public static final int BIG_NUMBER = 100000;
     public static final int HASHSET_CAPACITY = (int)Math.pow(2,10);
-    public static final int SEND_MESSAGE = 2000;
+    public static final int SEND_MESSAGE = 5000;
 
     public static final String getIpFromHosts(List<Host> hosts, int processId){
         return hosts.get(processId - 1).getIp();
