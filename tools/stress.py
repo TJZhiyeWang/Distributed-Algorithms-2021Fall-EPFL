a022261455150b4bea6010981b0f0cc1de577f8c
Zhiye Wang <493878030@qq.com>
2021-12-11 17:41:21 +0100

    Update stress.py

--- tools/stress.py
+++ tools/stress.py
@@ -363,8 +363,8 @@ if __name__ == "__main__":
     results = parser.parse_args()
 
     testConfig = {
-        'concurrency' : 8, # How many threads are interferring with the running processes
-        'attempts' : 8, # How many interferring attempts each threads does
+        'concurrency' : 0, # How many threads are interferring with the running processes
+        'attempts' : 0, # How many interferring attempts each threads does
         'attemptsDistribution' : { # Probability with which an interferring thread will
             'STOP': 0.48,          # select an interferring action (make sure they add up to 1)
             'CONT': 0.48,
