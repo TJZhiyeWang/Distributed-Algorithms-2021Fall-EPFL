15f636e672c54dd6b4253688535a4d0d6e02a96b
Zhiye Wang <493878030@qq.com>
2021-12-17 22:16:26 +0100

    Revert "delay"
    This reverts commit 5ec325c6fc1809a2969ed9c967c0b920432106d4.

--- src/main/java/cs451/Listener/Listener.java
+++ src/main/java/cs451/Listener/Listener.java
@@ -9,11 +9,6 @@ public abstract class Listener extends Thread implements Broadcast {
 
     @Override
     public void run(){
-        try {
-            Thread.sleep(10000);
-        }catch (InterruptedException e){
-            e.printStackTrace();
-        }
         while(flag){
             Record record = deliver();
 //            if (record != null) {
